cb08f8234f36872307f30faac749c94b03b3edb3
Filip <101202559+ClanyX@users.noreply.github.com>
2025-03-11 18:26:28 +0100

    Edit bg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -127,7 +127,7 @@
 <div class="flex flex-col justify-center items-center h-screen">
     <h1 class="text-4xl font-bold p-4">Guess the word</h1>
     <div>Your score: {score}</div>
-    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onclick="{onHintClick}">Hint 💡</button>
+    <button class="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded" onclick="{onHintClick}">Hint 💡</button>
     {#if statusText}
         <div>{statusText}</div>
         <button class="btn" bind:this={resetBtn} onclick={() => resetForm()}>Next word</button>
